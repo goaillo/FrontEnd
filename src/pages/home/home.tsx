a6bd59ae0f7d4fca0e5a52eb8a8d835ecb485c70
Goaillo <goaillo@DESKTOP-I9GAP25>
2023-09-04 12:42:33 -0400

    sort imports

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,56 +1,51 @@
-import React, { Component, useEffect } from 'react';
-import { Navigate } from 'react-router-dom';
-import axios from 'axios';
-import './home.css';
-import LoginHeader from '../header/header';
-import moment from 'moment';
+import React, { Component } from 'react'
+import { Navigate } from 'react-router-dom'
+import axios from 'axios'
+import moment from 'moment'
 
-interface IPostItem {
-  name: string; 
-  image_path: string;
-}
+import LoginHeader from '../header/header'
+
+import './home.css'
 
 class Home extends Component {
   // TODO better type handling
-  public state = {redirect: "", posts: [{name: "", image_path:"", start_date: "", end_date:""}]};
+  public state = { redirect: '', posts: [{ name: '', image_path: '', start_date: '', end_date: '' }] }
 
-  public fetchPosts = () => {
-    let componentContext = this;
-
+  public fetchPosts (): void {
     axios({
-      method: "get",
-      url: "post",
+      method: 'get',
+      url: 'post'
     })
-    .then(function (response) {
-      //handle success
-      var posts = []
-      for (let i = 0; i < response.data.length; i++) {
-        let start_date = moment(response.data[i].start_date);
-        let end_date = moment(response.data[i].end_date);
-        posts.push({
-          "name": response.data[i].post_name,
-          "image_path": response.data[i].image_path,
-          "start_date":  start_date.isValid() ? start_date.format("DD-MM-YYYY"): "",
-          "end_date": end_date.isValid() ? end_date.format("DD-MM-YYYY") : "",
-        })
-      }
-      componentContext.setState({posts: posts})
-    })
-    .catch(function (error) {
-      //handle error
-      console.error(error)
-    });
+      .then((response) => {
+        // handle success
+        const posts = []
+        for (let i = 0; i < response.data.length; i++) {
+          const startDate = moment(response.data[i].start_date)
+          const endDate = moment(response.data[i].end_date)
+          posts.push({
+            name: response.data[i].post_name,
+            image_path: response.data[i].image_path,
+            start_date: startDate.isValid() ? startDate.format('DD-MM-YYYY') : '',
+            end_date: endDate.isValid() ? endDate.format('DD-MM-YYYY') : ''
+          })
+        }
+        this.setState({ posts })
+      })
+      .catch(function (error) {
+        // handle error
+        console.error(error)
+      })
   }
 
-  public componentDidMount() {
+  public componentDidMount (): void {
     this.fetchPosts()
   }
 
-  public createPost = () => {
-    this.setState({redirect : (<Navigate to="/post" replace={true} />)});
-  };
+  public createPost (): void {
+    this.setState({ redirect: (<Navigate to="/post" replace={true} />) })
+  }
 
-  public render() {
+  public render (): React.ReactNode {
     return (
       <div className="Home">
         { this.state.redirect }
@@ -66,7 +61,7 @@ class Home extends Component {
             <ul className='postList'>
             {
               this.state.posts.map(post => {
-                if (post != null && post.name != "") {
+                if (post != null && post.name !== '') {
                   return (
                     <li key={`movie-${post.name}`} className='postCard'>
                       <div className='postTitle'>
@@ -74,10 +69,12 @@ class Home extends Component {
                       </div>
                       <img src={post.image_path} alt="new" className='postImage'/>
                       <div className='projectDates'>
-                        {post.start_date} :: { post.end_date != "" ? post.end_date : "..." }
+                        {post.start_date} :: { post.end_date !== '' ? post.end_date : '...' }
                       </div>
                     </li>
                   )
+                } else {
+                  return null
                 }
               })
             }
@@ -85,9 +82,8 @@ class Home extends Component {
           </div>
         </div>
       </div>
-    );
+    )
   }
 }
 
-
-export default Home;
+export default Home
